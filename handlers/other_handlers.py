8be70d3e71e7b31c0387fecd50bea76ff41a43b9
Kirill <kerlykiv@gmail.com>
2023-03-20 19:43:46 +0700

    echo bot v.0.1

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,1 +1,14 @@
-print('handlers')
+from aiogram import Router
+from aiogram.types import Message
+from lexicon.lexicon import LEXICON_RU
+
+router: Router = Router()
+
+
+@router.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.json(exclude_none=True, indent=4))
+    except TypeError:
+        await message.reply(text=LEXICON_RU['no_echo'])
